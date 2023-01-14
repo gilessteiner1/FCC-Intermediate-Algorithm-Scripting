723686ad6c876212134ecf8573d3d5d0effcde11
gilessteiner1 <gilessteiner1@gmail.com>
2023-01-14 14:45:46 -0500

    Update main.js
    Diff Two Arrays

--- main.js
+++ main.js
@@ -10,3 +10,17 @@ function sumAll(arr) {
   return total;
 }
 console.log(sumAll([1, 4])); //->10
+
+/* Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+Note: You can return the array with its elements in any order. */
+function diffArray(arr1, arr2) {
+  const newArr = [];
+  let mergedArr = arr1.concat(arr2);
+  for(let i=0; i<mergedArr.length; i++){
+    if(!(arr1.includes(mergedArr[i]) && arr2.includes(mergedArr[i]))){
+      newArr.push(mergedArr[i]);
+    }
+  }
+  return newArr;
+}
+console.log(diffArray([1, 2, 3, 5, 8], [1, 2, 3, 4, 5])); //-> [8,4]
